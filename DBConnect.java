8706cf855951b0d4fcd89ada7dc7a1cac5700b9c
Sani <forchu.cha@gmail.com>
2023-02-19 22:50:36 +0100

    Updated DBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,12 @@
+[ Album ]
+Hello Engineers,
+I wish to find out if anyone can assist me please.
+
+I have encountered, whilst trying to move a file to another server.
+
+Please can anyone assist with this challenge?
+
+I have changed the password authentication from No to yes but to no avail as you can see in the attached print-screens
+
+Many thanks,
+Steven
